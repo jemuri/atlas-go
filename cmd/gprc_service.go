b9a4f47719ebe838fc34021797a1ef8056eacbfe
stefan <songzhengjie1102@gmail.com>
2018-11-24 15:11:15 +0800

    integer

---
+++ cmd/gprc_service.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/jemuri/test-go/v6/utils/testmath"
+
+func main() {
+
+
+	testmath.GetNum(1,2)
+	print("hello,world!")
+}
---
+++ utils/interger.go
@@ -0,0 +1,5 @@
+package utils
+
+func Integer(a, b int) int {
+	return a * b
+}
